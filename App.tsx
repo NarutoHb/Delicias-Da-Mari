1dbc5e72e89ddc4aeff5c9e7882e720dde70ac9a
NarutoHb <narutomaitou@gmail.com>
2026-03-27 15:41:55 -0300

    Create App.tsx file for Delícias Da Mari app

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DeliciasDaMari: React.FC = () => {
+    return (
+        <div>
+            <h1>Delícias Da Mari</h1>
+            <p>Welcome to Delícias Da Mari!</p>
+        </div>
+    );
+};
+
+export default DeliciasDaMari;
