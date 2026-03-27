74f85be2932e05d80c45c843685abfffabc1b24a
NarutoHb <narutomaitou@gmail.com>
2026-03-27 20:35:07 -0300

    Delete src/App.tsx

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function App() {
-  return (
-    <div style={{ backgroundColor: 'pink', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <h1>TESTE: O SITE DA MARI ESTÁ VIVO! 🧁</h1>
-    </div>
-  );
-}
