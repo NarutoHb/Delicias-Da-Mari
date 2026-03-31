c247d0be1cc51161f4d32016843197c01a6b9abc
NarutoHb <narutomaitou@gmail.com>
2026-03-31 05:25:38 -0300

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev
-export default defineConfig({
-  plugins: [react()],
-  base: './', // Garante que os caminhos dos arquivos sejam relativos
-})
